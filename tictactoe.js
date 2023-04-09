474349a09644140ea7faf1f9d405a8fb11639e64
mauiman77 <boerbasher@protonmail.com>
2023-04-09 19:20:35 +0100

    Add turn module

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,21 @@
+const Turn = (() => {
+  const thisTurn = () => 1
+  const addTurn = newTurn => thisTurn + newTurn
+  return { thisTurn, addTurn }
+})()
+
 const Board = (() => {
   const layout = new Array(9)
+  const charChar = () => {
+    if (Turn.thisTurn % 2 === 0) {
+      Turn.addTurn(1)
+      return 'X'
+    } else {
+      Turn.addTurn(1)
+      return 'O'
+    }
+  }
+
   const createLayout = () => {
     const pageBoard = document.querySelector('.main')
     for (let i = 0; i < 9; i++) {
@@ -7,11 +23,21 @@ const Board = (() => {
       cell.className = 'cell'
       cell.className += ` ${i}`
       pageBoard.appendChild(cell)
+      cell.addEventListener('click', () => {
+        cell.textContent = charChar()
+      })
     }
   }
+
   return { layout, createLayout }
 })()
 
-const Player = (playerName, playerMarker) => {
-  return { playerName, playerMarker }
+const Player = (playerMarker) => {
+  return { playerMarker }
 }
+
+// ----
+
+Board.createLayout()
+const playerX = Player('X')
+const playerO = Player('O')
