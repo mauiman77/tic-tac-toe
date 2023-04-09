7c815d569679420fe211b1ec00a45b89358370dc
mauiman77 <boerbasher@protonmail.com>
2023-04-09 16:55:26 +0100

    Fix Board module

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,8 @@
-const Board = () => {
+const Board = (() => {
   const layout = new Array(9)
-}
+  const 
+  return {}
+})()
 
 const Player = (playerName, playerMarker) => {
   return { playerName, playerMarker }
