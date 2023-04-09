e5316e01878d6fc75379dd61c604331914e077e0
mauiman77 <boerbasher@protonmail.com>
2023-04-09 17:23:57 +0100

    Create createBoard function

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,15 @@
 const Board = (() => {
   const layout = new Array(9)
-  const 
-  return {}
+  const createLayout = () => {
+    const pageBoard = document.querySelector('.main')
+    for (let i = 0; i < 9; i++) {
+      const cell = document.createElement('div')
+      cell.className = 'cell'
+      cell.className += ` ${i}`
+      pageBoard.appendChild(cell)
+    }
+  }
+  return { layout, createLayout }
 })()
 
 const Player = (playerName, playerMarker) => {
