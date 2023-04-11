e8266a80497129f013c7b7f2982435c1b08bf103
mauiman77 <boerbasher@protonmail.com>
2023-04-11 13:30:03 +0100

    Make winning possible

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,18 @@
 const Score = (() => {
-  return []
+  const winningMoves = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 8], [1, 5, 9], [3, 5, 7]]
+
+  const insertTest = (id, marker) => {
+    for (let i = 0; i < winningMoves.length; i++) {
+      if (winningMoves[i].includes(id)) {
+        const thisId = winningMoves[i].indexOf(id)
+        Score.winningMoves[i][thisId] = marker
+        if (checkEqual(winningMoves[i])) {
+          console.log('WINNER')
+        }
+      }
+    }
+  }
+  return { winningMoves, insertTest }
 })()
 
 const Turn = (() => {
@@ -33,7 +46,10 @@ const Board = (() => {
       pageBoard.appendChild(cell)
       cell.addEventListener('click', () => {
         if (!cell.textContent) {
+          const cellId = Number(cell.getAttribute('id'))
+          console.log(cellId)
           cell.textContent = addMarker()
+          Score.insertTest(cellId, Turn.thisTurn())
           Turn.incrementTurn()
         }
       })
@@ -52,20 +68,7 @@ const Player = (playerMarker) => {
 Board.createLayout()
 const player1 = Player('X')
 const player2 = Player('O')
-const winningMoves = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 8], [1, 5, 9], [3, 5, 7]]
 
 function checkEqual (arry) {
   return arry.every((val, i, arr) => val === arr[0])
-}
-
-function insertTest (id, marker) {
-  for (let i = 0; i < winningMoves.length; i++) {
-    if (winningMoves[i].includes(id)) {
-      const thisId = winningMoves[i].indexOf(id)
-      winningMoves[i][thisId] = marker
-      if (checkEqual(winningMoves[i])) {
-        return 'yay!'
-      }
-    }
-  }
 }
