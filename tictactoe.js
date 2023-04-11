33fc3cacd27935c27353f367fb863d73799d6760
mauiman77 <boerbasher@protonmail.com>
2023-04-11 13:05:27 +0100

    Test array insert fn

--- tictactoe.js
+++ tictactoe.js
@@ -1,3 +1,7 @@
+const Score = (() => {
+  return []
+})()
+
 const Turn = (() => {
   let counter = 1
   const incrementTurn = () => {
@@ -5,44 +9,63 @@ const Turn = (() => {
   }
   const thisTurn = () => {
     if (counter % 2 === 0) {
-      incrementTurn()
-      return 'X'
-    } else {
-      incrementTurn()
       return 'O'
+    } else {
+      return 'X'
     }
   }
-  return ({ thisTurn })
+  return ({ thisTurn, incrementTurn })
 })()
 
 const Board = (() => {
-  const layout = new Array(9)
-  const charChar = () => {
+  // const layout = new Array(9)
+  const nextTurn = () => {
     const turn = Turn.thisTurn()
     return turn
   }
 
   const createLayout = () => {
     const pageBoard = document.querySelector('.main')
-    for (let i = 0; i < 9; i++) {
+    for (let i = 1; i < 10; i++) { // start at 1, for ease
       const cell = document.createElement('div')
       cell.className = 'cell'
-      cell.className += ` ${i}`
+      cell.setAttribute('id', `${i}`)
       pageBoard.appendChild(cell)
       cell.addEventListener('click', () => {
-        cell.textContent = charChar()
+        if (!cell.textContent) {
+          cell.textContent = nextTurn()
+          Turn.incrementTurn()
+        }
       })
     }
   }
 
-  return { layout, createLayout }
+  return { createLayout }
 })()
 
-const Player = (playerName) => {
-  return { playerName }
+const Player = (playerMarker) => {
+  const getMarker = () => playerMarker
+  return { getMarker }
 }
 
 // ----
 Board.createLayout()
-const playerX = Player('X')
-const playerO = Player('O')
+const player1 = Player('X')
+const player2 = Player('O')
+const winningMoves = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 8], [1, 5, 9], [3, 5, 7]]
+
+function checkEqual (arry) {
+  return arry.every((val, i, arr) => val === arr[0])
+}
+
+function insertTest (id, marker) {
+  for (let i = 0; i < winningMoves.length; i++) {
+    if (winningMoves[i].includes(id)) {
+      const thisId = winningMoves[i].indexOf(id)
+      winningMoves[i][thisId] = marker
+      if (checkEqual(winningMoves[i])) {
+        return 'yay!'
+      }
+    }
+  }
+}
